57abb9bfbeda7756b2ba01da3c268ead462e8be6
Santiago29-6 <chavarrokevin29@gmail.com>
2024-11-23 18:41:47 -0500

    Subiendo proyecto inicial
    Se sube el proyecto

---
+++ src/main/java/com/cine/cine_remake/CineRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.cine.cine_remake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineRemakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cine/cine_remake/CineRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.cine_remake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
